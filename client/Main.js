7ff38a898463dabc7a2eb6f3584cae5d65819abd
Sarah Alli <sarah@sah.nyc>
2023-07-27 12:22:42 -0400

    updated boilerplate

--- client/Main.js
+++ client/Main.js
@@ -1,11 +1,9 @@
 import React from "react";
-import PokemonList from "./PokemonList";
 
 const Main = () => {
   return (
     <div id="main" className="row container">
       <h1>Pokedex</h1>
-      <PokemonList />
     </div>
   );
 };
